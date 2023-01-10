f3afe769bbedc465a78a4689096c3f630961a539
VuATM <vuhtse140902@gmail.com>
2023-01-10 23:29:08 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { environment } from "../environments/environment";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from 'firebase/app';
+import { MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService, DialogService]
 })
 export class AppComponent {
   title = 'AdminWebApp';
+  message: any = null;
+  constructor() { }
+  ngOnInit(): void {
+    this.requestPermission();
+    this.listen();
+  }
+  requestPermission() {
+    const app = initializeApp(environment.firebase);
+    const messaging = getMessaging(app);
+    getToken(messaging,
+      { vapidKey: environment.firebase.vapidKey }).then(
+        (currentToken) => {
+          if (currentToken) {
+            console.log("Hurraaa!!! we got the token.....");
+            console.log(currentToken);
+            localStorage.setItem("FCM", currentToken);
+          } else {
+            console.log('No registration token available. Request permission to generate one.');
+          }
+        }).catch((err) => {
+          console.log('An error occurred while retrieving token. ', err);
+        });
+  }
+  listen() {
+    const app = initializeApp(environment.firebase);
+    const messaging = getMessaging(app);
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      this.message = payload;
+    });
+  }
 }
